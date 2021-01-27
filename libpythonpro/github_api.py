f8b9333b8e9778548360a780a3d3e5b9016cb0de
Victor Baptista Lemos <victorbaptistalemos@icloud.com>
2021-01-27 04:23:38 +0000

    Instalação de Libs com PIP

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+
+    :param usuario: str com o nome do usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resposta = requests.get(url)
+    return resposta.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('victorbaptistalemos'))
