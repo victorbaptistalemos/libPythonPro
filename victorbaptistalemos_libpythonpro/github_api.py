139d4cfe63dca4c70dd143f0d19b26e4f1cb87f3
Victor Baptista Lemos <victorbaptistalemos@icloud.com>
2021-02-01 07:38:10 +0000

    Tópicos adicionais no README.md e novos testes.
    close #15

--- victorbaptistalemos_libpythonpro/github_api.py
+++ victorbaptistalemos_libpythonpro/github_api.py
@@ -11,7 +11,3 @@ def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resposta = requests.get(url)
     return resposta.json()['avatar_url']
-
-
-if __name__ == '__main__':
-    print(buscar_avatar('victorbaptistalemos'))
