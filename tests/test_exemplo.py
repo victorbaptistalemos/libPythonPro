0158d2ba5790148c12fb8e7bc4b7cef471721c89
Victor Baptista Lemos <victorbaptistalemos@icloud.com>
2021-02-16 04:29:24 +0000

    Isolamento de Imports

--- tests/test_exemplo.py
+++
@@ -1,5 +0,0 @@
-from victorbaptistalemos_libpythonpro.github_api import buscar_avatar
-
-
-def test_buscar_avatar():
-    assert buscar_avatar('victorbaptistalemos') == 'https://avatars.githubusercontent.com/u/52052351?v=4'
--- tests/test_spam/test_users.py
+++ tests/test_spam/test_users.py
@@ -2,13 +2,17 @@ from victorbaptistalemos_libpythonpro.spam.user import User
 
 
 def test_add_users(session):
-    user = User(name='Victor')
+    user = User('Victor', 'victorbaptistalemos@icloud.com')
     session.add_user(user)
     assert isinstance(user.id, int)
 
 
 def test_list_users(session):
-    users = [User(name='Victor'), User(name='Baptista'), User(name='Lemos')]
+    users = [
+        User('Victor', 'victorbaptistalemos@icloud.com'),
+        User('Baptista', 'victorbaptistalemos@gmail.com'),
+        User('Lemos', 'victorbaptistalemos@ymail.com')
+    ]
     for user in users:
         session.add_user(user)
     assert users == session.list_users()
